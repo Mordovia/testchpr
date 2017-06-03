4501c7c7500dae19a3595ef1f36f2cd7ff7274ba
Mordovia <solosbor@gmail.com>
2017-06-03 16:25:24 +0300

    Update engine.js

--- engine.js
+++ engine.js
@@ -34,41 +34,24 @@ loaded: false
 };
 
 var getUserName = function () {//описываем функцию, которая обращается к API
-  VK.api('users.get', {"fields" : "photo_50"}, function (data) {//метод users.get - это тоже функция, которая выводит что-то, обращаясь к АПИ %(
-//console.log(data);
-user['name'] = data.response[0].first_name;//обратились к ВК API - получили ответ, который записали в параметр name переменной user
-user['avatar'] = data.response[0].photo_50;//обратились к ВК API - получили ответ, который записали в параметр avatar переменной user
+  	VK.api('users.get', {"fields" : "photo_50"}, function (data) {//метод users.get - это тоже функция, которая выводит что-то, обращаясь к АПИ %(
+	//console.log(data);
+	user['name'] = data.response[0].first_name;//обратились к ВК API - получили ответ, который записали в параметр name переменной user
+	user['avatar'] = data.response[0].photo_50;//обратились к ВК API - получили ответ, который записали в параметр avatar переменной user
 
- document.avatar.src = user['avatar'] //определили картинку с именем avatar и ссылкой из ВК Апи, которую потом выводим в HTML по имени
-// document.write ("Привет, " + user['name']);
-}),
- VK.api('groups.getById', {'group_ids': '121807904', 'fields' : 'photo_50'}, function (data) {
+ 	document.avatar.src = user['avatar'] //определили картинку с именем avatar и ссылкой из ВК Апи, которую потом выводим в HTML по имени
+	// document.write ("Привет, " + user['name']);
+	}),
+ 	VK.api('groups.getById', {'group_ids': '121807904', 'fields' : 'photo_50'}, function (data) {
 	group['groupname'] = data.response[0].name;
 //	group['group_avatar'] = data.response[0].photo_50;
 	user['loaded'] = true;
-document.getElementById('hello').innerHTML='Привет, '+ user['name'] + ', хочешь получить чайный прогноз от ' + group['groupname'] + '?';//ищем в html-коде кусок с тегом "hello", после чего записываем в него текст 
+	document.getElementById('hello').innerHTML='Привет, '+ user['name'] + ', хочешь получить чайный прогноз от ' + group['groupname'] + '?';//ищем в html-коде кусок с тегом "hello", после чего записываем в него текст 
    	})
   };
   function exit() {//функция выхода - перебрасывает на главную страницу ВК
 		    parent.window.location.href = 'http://vk.com/';
 }
-VK.api('market.get', {
-							'owner_id': '-121807904', 
-							'album_id': '16',
-                    }, function(data) {
-//console.log(data);                        
-var a = data.response.items;
-	function compareRandom(a, b) {
-	return Math.random() - 0.5;
-	}
-a.sort(compareRandom);
-//console.log(a[0], a[1], a[2], a[3]);
-                       market['descript0'] = a[0].title;
-                       market['photo_0'] = a[0].thumb_photo;
-                       url['0'] = 'http://vk.com/market-121807904?w=product-121807904_' + a[0].id + '%2Fquery';
-console.log(url['0']);
-                       market['loaded'] = true;
-});
   var status = false;	       
 VK.init(function() { //с этого мы начинаем работу с ВКонтакте API
       status = true; //меняем состояние переменной
