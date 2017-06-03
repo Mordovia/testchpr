87e10244bd395e1933e50c2e6d1ca5ff7c747da3
Mordovia <solosbor@gmail.com>
2017-06-03 19:44:16 +0300

    Update engine.js

--- engine.js
+++ engine.js
@@ -68,10 +68,10 @@ function drawTable() {//рисуем таблицу
                        market['descript'+'i'] = a[i].title;
                        market['photo_'+'i'] = a[i].thumb_photo;
                        url['i'] = 'http://vk.com/market-121807904?w=product-121807904_' + a[i].id + '%2Fquery';
-		       }
-			console.log(url['1']);
-			console.log(market['descript'+'1']);
-			
+		       
+			//console.log(url['i']);
+			console.log(market);
+			}
                 market['loaded'] = true;
 		       
 					
