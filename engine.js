2eca61ac9bb9b7789af6fe5177be2fc9b1175496
Mordovia <solosbor@gmail.com>
2017-06-03 20:54:00 +0300

    Update engine.js

--- engine.js
+++ engine.js
@@ -107,10 +107,10 @@ function drawTable() {//рисуем таблицу
 		var img = new Image;
 		img.width = 100;
 		img.height = 100;
-		for (n=0;n<6;n++){
+		for (n=1;n<4;n++){
 		img.src = market['photo_'+n];
 		document.getElementById(n+'1').appendChild(img);
-		document.getElementById(n+'2').innerHTML =  market['descript0'];
+		document.getElementById(n+'2').innerHTML =  market['descript'+n];
 		}
 })};
   var status = false;	       
