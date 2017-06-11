6df501909f2fbfd49ea45cbc0f120a4825d46398
Mordovia <solosbor@gmail.com>
2017-06-11 13:51:55 +0300

    Create engine.js

--- engine.js
+++ engine.js
@@ -106,7 +106,7 @@ function marketGet(){
 		drawTable();
 		})}
 function clearScreen(){
-	var tagName = document.getElementsByTagName('name');
+	var tagName = document.getElementsByTagName('input');
 	for(i = 0; i < tagName.length; i++){
 	console.log(tagName);
 	}
