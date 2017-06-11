366e4852567f38ccb4e4621959822db58dc30cf4
Mordovia <solosbor@gmail.com>
2017-06-11 12:57:24 +0300

    Create market.js

--- market.js
+++ market.js
@@ -1,6 +1,18 @@
+var albums = {
+name: '',
+id: '',
+};
 function albumsGet(){
 	VK.api('market.getAlbums', {
 							'owner_id': '-121807904', 
 							}, function(data) {
-					console.log(data);                        
+					console.log(data);
+		var a = data.response.items;
+		for (var n=0; n<a.count; n++){
+                       	name = a.title;
+                       	id = a.id;
+			console.log(name);
+			console.log(id);
+			
+              }
 })}
