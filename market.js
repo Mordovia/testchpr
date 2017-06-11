2d03e6334528511f48ff4de418f0ef2163083dad
Mordovia <solosbor@gmail.com>
2017-06-11 15:01:28 +0300

    Create market.js

--- market.js
+++ market.js
@@ -4,6 +4,7 @@ id: '',
 };
 function albumsGet(){
 	clearScreen();
+	document.getElementById('hello').innerHTML = "ID группы:	Имя группы";
 	VK.api('market.getAlbums', {
 							'owner_id': '-121807904', 
 							}, function(data) {
@@ -13,6 +14,7 @@ function albumsGet(){
 		for (var n=0; n<a.length; n++){
                        	name = a[n].title;
                        	id = a[n].id;
+			document.getElementById('button').innerHTML = id+":	"+name;
 		//	console.log(name);
 		//	console.log(id);
 			
