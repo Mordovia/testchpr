f5690245e51dd82900f58c3d8186b9b61ec048c1
Mordovia <solosbor@gmail.com>
2017-06-15 20:57:20 +0300

    Create market.js

--- market.js
+++ market.js
@@ -16,7 +16,7 @@ function albumsGet(){
 			//var text = id+':	'+name+'<br>'
 		//console.log(a);
 		//console.log(name);
-		document.getElementById('exit').innerHTML += '<p style=" margin-top: 0px; margin-bottom: 0px;"><input id="checkbox'+n+" 'type="checkbox">'+id+': '+name+'<br></p>';
+		document.getElementById('exit').innerHTML += '<p style=" margin-top: 0px; margin-bottom: 0px;"><input id="checkbox'+n+'" type="checkbox">'+id+': '+name+'<br></p>';
 			if (document.getElementById('checkbox'+n).checked)
 				conole.log("Флажок установлен"); 
 			else
