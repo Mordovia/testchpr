ca2afabedea2e2d2af285e9f46c197ffbbb5ff98
Mordovia <solosbor@gmail.com>
2017-06-15 19:10:12 +0300

    Create market.js

--- market.js
+++ market.js
@@ -16,7 +16,7 @@ function albumsGet(){
 			//var text = id+':	'+name+'<br>'
 		//console.log(a);
 		//console.log(name);
-		document.getElementById('exit').innerHTML += '<p>'+name+'<br></p>';
+		document.getElementById('exit').innerHTML += '<p><small>'+name+'<br></small></p>';
 			//console.log(name+': '+id);
 			}
 
