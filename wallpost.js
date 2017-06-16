cd97fd3007181cfadc1ba78e7d6cd28cac95c5f0
Mordovia <solosbor@gmail.com>
2017-06-16 12:03:42 +0300

    Create wallpost.js

---
+++ wallpost.js
@@ -0,0 +1,22 @@
+function wallGet(){
+	clearScreen();
+		VK.api('wall.get', {
+							'owner_id': '-121807904', 
+							}, function(data) {
+					console.log(data);
+		/* a = data.response.items;
+		//console.log(a);
+		for (var n=0; n<a.length -1; n++){
+                       	var name = [];
+			name = a[n].title;
+                       	id = a[n].id;
+			
+			//var text = id+':	'+name+'<br>'
+		//console.log(a);
+		//console.log(name);
+		document.getElementById('exit').innerHTML += '<p style=" margin-top: 0px; margin-bottom: 0px;"><input id="checkbox'+n+'" type="checkbox" checked>'+id+': '+name+'<br></p>';
+		}
+		buttonCreation3();	
+
+		
+}*/)}
