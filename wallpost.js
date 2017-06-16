7bcf26b3df0c2683f6da29deeb1ae2afe332cac6
Mordovia <solosbor@gmail.com>
2017-06-16 13:06:07 +0300

    Update wallpost.js

--- wallpost.js
+++ wallpost.js
@@ -3,7 +3,7 @@ function wallGet(){
 		VK.api('wall.get', {
 							'owner_id': '-121807904',
 							'count': '1',
-							'offset': '3',
+							'offset': getRandomInt(a,b),
 							}, function(data) {
 					console.log(data);
 			var a = 0;
