a6d2f00c2b51965c1cd4302df0ac9fcebda67f15
Mordovia <solosbor@gmail.com>
2017-06-17 09:09:24 +0300

    Update wallpost.js

--- wallpost.js
+++ wallpost.js
@@ -33,7 +33,7 @@ function wallGet(){
 			if(a[0].type == "photo"){
 				console.log('картинка');				
 			   img[0] = new Image;
-			   img[0].src = a[0].photo_75;
+			   img[0].src = a[0].photo.photo_75;
 				console.log(img[0].src);
 			   document.getElementById('button').appendChild(img[0]);
 				}
