13fb7b9511694460b45c59194a27d57cac33d3bd
Mordovia <solosbor@gmail.com>
2017-06-17 20:11:57 +0300

    Update wallpost.js

--- wallpost.js
+++ wallpost.js
@@ -5,7 +5,7 @@ var owner_id;
 var id;
 var text;
 function getRandomInt(min,max){
-	return Math.floor(min + Math.random() * (max + 1 - min);
+	return Math.floor(min + Math.random() * (max + 1 - min));
 }
 
 function wallGet(){
