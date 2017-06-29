ac64b76ee10bfff88f3a97b6d1b6787b6eced276
Mordovia <solosbor@gmail.com>
2017-06-29 16:42:27 +0300

    Update wallpost.js

--- wallpost.js
+++ wallpost.js
@@ -34,7 +34,8 @@ function wallGet(){
 	 	owner_id = x[0].owner_id;
 	}
 		})});
-		document.getElementById('market').innerHTML = '<table><tr><td><input value = "Перейти к записи" type = "button" onclick = look();></td><td><input value = "Опубликовать" type = "button" onclick = "postIt();"></td><td><input value = " >> Получить другую запись" type = "button" onclick = "newPost();"></td></tr></table>';
+		document.getElementById('newPrognoz').innerHTML = '<table><tr><td><input value = "Перейти к записи" type = "button" onclick = look();></td><td><input value = "Опубликовать" type = "button" onclick = "postIt();"></td><td><input value = " >> Получить другую запись" type = "button" onclick = "newPost();"></td></tr></table>';
+		backToMainMenu();
 }
 function newPost(){
 //path = '';
