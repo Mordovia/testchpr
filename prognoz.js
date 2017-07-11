533f9593dcea4a47a88215ce1b615390c543193b
Mordovia <solosbor@gmail.com>
2017-07-11 16:51:57 +0300

    Update prognoz.js

--- prognoz.js
+++ prognoz.js
@@ -109,8 +109,8 @@ function marketGet(){
 		img[n].href = url[n];
 		}
 		todayImage = new Image;
-		todayImage.width = 50;
-		todayImage.height = 50;
+		todayImage.width = 75;
+		todayImage.height = 75;
 		todayImage.src = market['photo_1'];
 		todayImage.href = url['1'];
 		drawTableWeek();
