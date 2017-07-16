9b0f4fc6b48c453f171724fa527c1cc1b2805982
Mordovia <solosbor@gmail.com>
2017-07-16 21:46:09 +0300

    Update wallpost.js

--- wallpost.js
+++ wallpost.js
@@ -95,6 +95,7 @@ window.open('https://vk.com/vintagesbor?w=wall'+owner_id+'_'+id);
 }
 function selectData(){
 	document.getElementById('container1').style.width = "94%";
+	document.getElementById('container2').style.width = "0";
 	document.getElementById('newPrognoz').innerHTML = '<table><tr><td><input type="datetime-local" id = "dataSelect"></td><td><input value = "Опубликовать в выбранное время" type="button" onclick = postItSelected();></td></tr><tr><td><input value = "Опубликовать сейчас" type = "button" onclick = postItNow();></td><td><input value = "Опубликовать в случайное время" type = "button" onclick = "postItRandom();"></td></tr></table>';
 }
 function postItSelected(){
