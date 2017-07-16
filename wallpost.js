52ee8dd44917c79a88c52eaa378258d7c9d107f1
Mordovia <solosbor@gmail.com>
2017-07-16 20:55:53 +0300

    Update wallpost.js

--- wallpost.js
+++ wallpost.js
@@ -94,7 +94,8 @@ function look(){
 window.open('https://vk.com/vintagesbor?w=wall'+owner_id+'_'+id);
 }
 function selectData(){
-document.getElementById('newPrognoz').innerHTML = '<table><tr><td><input type="datetime-local" id = "dataSelect"></td><td><input value = "Опубликовать в выбранное время" type="button" onclick = postItSelected();></td></tr><tr><td><input value = "Опубликовать сейчас" type = "button" onclick = postItNow();></td><td><input value = "Опубликовать в случайное время" type = "button" onclick = "postItRandom();"></td></tr></table>';
+	document.getElementById('container1').style.width = "100%";
+	document.getElementById('newPrognoz').innerHTML = '<table><tr><td><input type="datetime-local" id = "dataSelect"></td><td><input value = "Опубликовать в выбранное время" type="button" onclick = postItSelected();></td></tr><tr><td><input value = "Опубликовать сейчас" type = "button" onclick = postItNow();></td><td><input value = "Опубликовать в случайное время" type = "button" onclick = "postItRandom();"></td></tr></table>';
 }
 function postItSelected(){
 	var str = new String;
