d52615bd53d6ad61eb28f17918fcf53345325fe3
Mordovia <solosbor@gmail.com>
2017-07-16 23:27:05 +0300

    Update prognoz.js

--- prognoz.js
+++ prognoz.js
@@ -200,7 +200,7 @@ function createTableWeek(){
 	var caption = document.createElement('caption');
 	caption.id = "00";
 	table.appendChild(caption);
-	for (var i=0; i<7; i++){
+	for (var i=0; i<8; i++){
 		var tr = document.createElement('TR');
     	for (var j=0;j<4;j++)
 		{
