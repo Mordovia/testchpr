62b565dc4b05594feee9e1b7ab410d57add7e284
Mordovia <solosbor@gmail.com>
2017-07-18 11:39:08 +0300

    Update prognoz.js

--- prognoz.js
+++ prognoz.js
@@ -248,12 +248,12 @@ function onClickCell(event){//функция события
 	for (n=0; n<8; n++){
 		for (m=0; m<3; m++){
 		if (event.target.id = n+m){
-			document.getElementById('t00').innerHTML = day[n].toLocaleString("ru", options);
-			document.getElementById('t10').innerHTML =  todayImage[n].title;
 			document.getElementById('t'+n+m).innerHTML =  "";
 			document.getElementById('t30').innerHTML = 'Утро';
 			document.getElementById('t31').innerHTML = 'День';
 			document.getElementById('t32').innerHTML = 'Вечер';
+			document.getElementById('t00').innerHTML = day[n].toLocaleString("ru", options);
+			document.getElementById('t10').innerHTML =  todayImage[n].title;
 			document.getElementById('t20').appendChild(todayImage[n]);
 			document.getElementById('t40').appendChild(img[n]);
 			document.getElementById('t50').innerHTML =  market['descript'+n];
