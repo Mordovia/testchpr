01c6cbc95c42b1912c76f629250bab41eca02e62
Mordovia <solosbor@gmail.com>
2017-07-18 17:39:49 +0300

    Update prognoz.js

--- prognoz.js
+++ prognoz.js
@@ -293,7 +293,7 @@ function postItOnWall(){
 		});
 	});*/
 	html2canvas($('header'), {
-		allowTaint: true,
+		allowTaint: false,
 		taintTest: false,
 		onrendered: function(canvas) {
 			var img = canvas.toDataURL()
