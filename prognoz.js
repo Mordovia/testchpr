62c0218b168bc082e0a75a1c2929bf49e68b0950
Mordovia <solosbor@gmail.com>
2017-07-23 10:06:17 +0300

    Update prognoz.js

--- prognoz.js
+++ prognoz.js
@@ -105,6 +105,7 @@ var buttonCreation2 = function(){
 	var pprg = document.createElement('input')
 	pprg.id = 'pprg'
 	pprg.type = 'button'
+	pprg.img.src = '/res/4p.png'
 	pprg.value = 'Опубликовать прогноз'
 	pprg.setAttribute('onclick', 'postItOnWall();')
 	postPrognoz.appendChild(pprg);
