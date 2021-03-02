136d228c6e5c60c8b94235f3284b28aedc1ea798
Mordovia <solosbor@gmail.com>
2021-03-02 16:42:08 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    # Путь к файлу
+    $file_name = "goods.txt";
+    
+    # Считываем информацию по строкам
+    $data = file( $file_name );
+?>
+<table border="1">
+<?php
+    # В цикле обходим массив данных
+    foreach( $data as $value ):
+    
+        # Разбиваем строку по ','
+        $value = explode( ",", $value );
+?>
+    <tr>
+        <td><?=$value[0]?></td>
+        <td><?=$value[1]?></td>
+        <td><?=$value[2]?></td>
+    </tr>
+<?php
+    endforeach;
+?>
+</table>
+echo "</table>";
